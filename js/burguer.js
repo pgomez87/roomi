a2a84830cbab606a83424cc4b26654cdeaa9ce84
JPablo <JPablo@MacBook-de-Pablo.local>
2020-11-07 18:00:36 -0600

    Actualizacion paginas Andres, creacion menu hamburguesa

---
+++ js/burguer.js
@@ -0,0 +1,14 @@
+'strict'
+
+const navSlide = () => {
+    const iconBurguer = document.querySelector('.icon-burguer');
+    const burguerMenu = document.querySelector('.burguer-menu');
+
+
+    iconBurguer.addEventListener('click', () => {
+        burguerMenu.classList.toggle('burguer-menu-active');
+    });
+
+}
+
+navSlide();
