ee11ac8dd554e979ccc699d7e78df362fdd83a0c
JPablo <JPablo@MacBook-de-Pablo.local>
2020-11-11 18:33:05 -0600

    updated nav dashboard-admi

--- js/calificaciones-controlador.js
+++ js/calificaciones-controlador.js
@@ -1,24 +1,41 @@
 'use strict';
 
+
 const boton_enviar = document.querySelector('#btn-enviar');
+const radio1 = document.getElementById('rbt-pesimo').check;
+const radio2 = document.getElementById('rbt-mal');
+const radio3 = document.getElementById('rbt-regular');
+const radio4 = document.getElementById('rbt-bien');
+const radio5 = document.getElementById('rbt-excelente');
+
+const obtener_datos = () => {
+    let pesimo = radio1.value;
+    let mal = radio2.value;
+    let regular = radio3.value;
+    let bien = radio4.value;
+    let excelente = radio5.value;
+
+    console.log(radio1);
+    console.log(radio2);
+    console.log(radio3);
 
 
-function validar() {
-    var valid = false;
-    var x = document.querySelector('#form-eva');
+    const validar = () => {
+        if ((radio1 == '') && (radio2 == '') && (radio3 == '') && (radio4 == '') && (radio5 == '')) {
+            Swal.fire({
+                'title': 'No se pudo enviar el reporte',
+                'icon': 'warning',
+                'text': 'Por favor revise los campos resaltados'
+            });
+        } else {
+            Swal.fire({
+                'title': 'El reporte ha sido enviado',
+                'icon': 'success',
+                'text': 'Nos pondremos en contacto con usted lo antes posible'
+            });
+        };
+    };
 
-    for (var i = 0; i < x.lenght; i++) {
-        if (x[i].checked) {
-            valid = true;
-            break;
-        }
-    }
-    if (valid) {
-        alert("Validación de exito");
-    } else {
-        alert("Please Select uno");
-        return false
-    }
-}
+};
 
-boton_enviar.addEventListener('click', validar);
+boton_enviar.addEventListener('click', obtener_datos);
