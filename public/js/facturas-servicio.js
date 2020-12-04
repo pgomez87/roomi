32728af40fa3fcd69b923dfaff86fed4c78be6c5
AndresJaen <74122497+AndresJaen@users.noreply.github.com>
2020-12-03 22:36:30 -0600

    quito el js que se creó por la migracion a folders api-public

--- public/js/facturas-servicio.js
+++ public/js/facturas-servicio.js
@@ -2,6 +2,7 @@
 
 const registrarFacturas = async(nombre, fecha, tipo, proveedor, porcentaje) => {
     await axios({
-
+        method: 'post',
+        url: 'http://localhost:3000/api/'
     })
 }
