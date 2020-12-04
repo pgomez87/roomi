dd961a7db4dbe430dc4e6bc340053fd5df9dbbca
AndresJaen <74122497+AndresJaen@users.noreply.github.com>
2020-12-04 00:28:34 -0600

    mucha vara

--- public/js/facturas-servicio.js
+++ public/js/facturas-servicio.js
@@ -3,6 +3,18 @@
 const registrarFacturas = async(nombre, fecha, tipo, proveedor, porcentaje) => {
     await axios({
         method: 'post',
-        url: 'http://localhost:3000/api/'
-    })
-}
+        url: 'http://localhost:3000/api/registrar-factura',
+        responseType: 'json',
+        data: {
+            nombre: nombre,
+            fecha: fecha,
+            tipo: tipo,
+            proveedor: proveedor,
+            porcentaje: porcentaje
+        }
+    }).then((response) => {
+        alert('se ha registrado la factura')
+    }).catch((response) => {
+        alert('no se ha podido registrar nada')
+    });
+};
