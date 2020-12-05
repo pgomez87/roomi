6eca35b9b2e2e3d0e5d24b02fbb2bb4b6b565508
AndresJaen <74122497+AndresJaen@users.noreply.github.com>
2020-12-05 04:01:08 -0600

    formulario para noticias hecho
    hay que cambiar la foto por algo que sea más relacionado a noticias.

--- public/js/noticias-coordinador-controlador.js
+++ public/js/noticias-coordinador-controlador.js
@@ -1,19 +1,13 @@
 'uset strict';
 
 let btnNoticias = document.querySelector('#agregar-noticias');
-let btnAgregarNot = document.querySelector('#agregarNoticia')
 let cuadro = document.querySelector('#box-noticias');
 
 const abrirForm = () => {
-    cuadro.classList.add('abrir-form');
-}
-
-const cerrarForm = () => {
-    cuadro.classList.remove('abrir-form');
+    location.replace('formulario-noticias.html');
 }
 
 
 
 
 btnNoticias.addEventListener('click', abrirForm);
-btnAgregarNot.addEventListener('click', cerrarForm);
