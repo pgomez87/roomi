26834104c5466129fa526e37dfa2e2da743eb6ff
AndresJaen <74122497+AndresJaen@users.noreply.github.com>
2020-12-05 20:23:21 -0600

    re-upload

--- public/js/formulario-noticias-controlador.js
+++ public/js/formulario-noticias-controlador.js
@@ -1,10 +1,14 @@
 'use strict';
 
 let btnAgregarNoticia = document.querySelector('#agregarNoticia');
+let tituloNoticia = document.querySelector('#tituloNoticia').value;
+let textoNoticia = document.querySelector('#txtNoticias').value;
+let imagenNoticia = document.querySelector('#imgNoticias').src;
+
 
 
 const prueba = () => {
-    alert('funciona');
+    console.log(tituloNoticia, textoNoticia);
 }
 
 
