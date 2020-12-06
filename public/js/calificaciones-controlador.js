36932a9dbdffc7bb5f6d4c852f6ab4f0ad58f9d6
Ian-Asch <iascht@ucenfotec.ac.cr>
2020-12-06 16:04:48 -0600

    calificaciones

--- public/js/calificaciones-controlador.js
+++ public/js/calificaciones-controlador.js
@@ -3,5 +3,32 @@
 const lista_cards = document.querySelectorAll('.card');
 
 lista_cards.forEach(card => {
-    let lista_forms = document.querySelectorAll('form');
+    let card_button = card.querySelector('button');
+    let error = false;
+
+    card_button.addEventListener('click', () => {
+
+        let lista_forms = card.querySelectorAll('form');
+    
+        lista_forms.forEach(form => {
+            let lista_label = form.querySelectorAll('label');
+            if (form.querySelector('input[type=radio]:checked')) {
+                lista_label.forEach(label => {
+                    label.classList.remove('error');
+                });
+            } else {
+                lista_label.forEach(label => {
+                    label.classList.add('error');
+                });
+
+                error = true;
+            }
+        });
+
+        if (error == true) {
+            // swal.fire
+        } else {
+            // swal.fire
+        }
+    });  
 });
