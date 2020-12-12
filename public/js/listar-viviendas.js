abf6bc35adaef51abdb00151cc89b11b492faba9
AndresJaen <74122497+AndresJaen@users.noreply.github.com>
2020-12-12 02:05:35 -0600

    Update listar-viviendas.js

--- public/js/listar-viviendas.js
+++ public/js/listar-viviendas.js
@@ -6,49 +6,42 @@ let idVivienda;
 const mostrarViviendas = async() => {
     let viviendasDisp = await listar_viviendas();
     let i = 0;
+    let a = 0;
 
     viviendasDisp.forEach((vivienda) => {
         for (i; i < viviendasDisp.length; i++) {
-            console.log(i);
-            let div1 = document.createElement('div');
-            div1.className = 'casa-individual';
-            div1.id = vivienda._id;
-            idVivienda = vivienda._id;
-            div1.innerHTML = `<h2>${vivienda.nombre}</h2>
+            console.log('tucan')
+        }
+        // console.log(i);
+
+        let div1 = document.createElement('div');
+        div1.className = 'casa-individual';
+        div1.id = vivienda._id;
+        idVivienda = vivienda._id;
+        div1.innerHTML = `<h2>${vivienda.nombre}</h2>
             <p>${vivienda.descripcion}</p>
             <img src="${vivienda.imagen}">
-            <button type="button"  class="btn-ver-vivienda" id="btn-ver-vivienda-${i}">Ver mas informacion</button>`;
-            localStorage.setItem('objetoPrueba', JSON.stringify(idVivienda));
-            contViviendas.appendChild(div1);
-        }
-        //crear divs
-
+            <button type="button"  class="btn-ver-vivienda" id="btn-ver-vivienda">Ver mas informacion</button>`;
+        localStorage.setItem('objetoPrueba', JSON.stringify(idVivienda));
+        contViviendas.appendChild(div1);
     });
+
+
 
     const prueba = () => {
         console.log('shiiiiiiiiiiiiiit');
     }
 
-    let shit = document.querySelector(`#btn-ver-vivienda-2`);
-
-    shit.addEventListener('click', prueba);
-    console.log(shit);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    for (a; a < i; a++) {
+        let btnID = document.querySelectorAll(`#btn-ver-vivienda`);
+        btnID[a].addEventListener('click', prueba);
+        if (a === viviendasDisp[a]) {
+            console.log(`esto es ${a}, y esto es ${i}`);
+        } else {
+            console.log('shit');
+        }
+        // console.log(a);
+    }
 };
 
 
