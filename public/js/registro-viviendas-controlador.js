21d89edaaa3b95dac9baa0acc11b9460f3bdd75d
rgonzalezs93 <71414956+rgonzalezs93@users.noreply.github.com>
2020-12-11 19:19:06 -0600

    Update registro-viviendas-controlador.js

--- public/js/registro-viviendas-controlador.js
+++ public/js/registro-viviendas-controlador.js
@@ -16,13 +16,24 @@ const input_sala = document.querySelector('#checkbox-sala');
 const input_jardin = document.querySelector('#checkbox-jardin');
 const input_garaje = document.querySelector('#checkbox-garaje');
 
+let widgetCloudinary = cloudinary.createUploadWidget({
+    cloudName: 'imagenes3d',
+    uploadPreset: 'preset_3d'
+
+}, (err, result) => {
+    if (!err && result && result.event === 'success') {
+        console.log('Imagen subida con éxito', result.info);
+        input_imagen.src = result.info.secure_url;
+    }
+});
+
 const limpiar = () => {
     input_nombre.value = '';
     input_costo.value = '';
     input_capacidad.value = '';
     input_descripcion.value = '';
     input_direccion.value = '';
-    input_imagen.value = '';
+    input_imagen.src = '';
     input_habitaciones.value = '';
     input_bannos.value = '';
     input_cocina.value = '';
@@ -39,7 +50,7 @@ const obtener_datos = () => {
     let capacidad = input_capacidad.value;
     let descripcion = input_descripcion.value;
     let direccion = input_direccion.value;
-    let imagen = input_imagen.value;
+    let imagen = input_imagen.src;
     let habitaciones = input_habitaciones.value;
     let bannos = input_bannos.value;
     let cocina = input_cocina.value;
@@ -62,11 +73,11 @@ const obtener_datos = () => {
 
 const validar = () => {
     let error = false;
-    let regex_nombre = /^[a-zA-Z ]+$/;
+    //let regex_nombre = /^[a-zA-Z ]+$/;
     let regex_costo = /^[0-9]+$/;
     let regex_capacidad = /^[0-9]/;
-    let regex_direccion = /^[a-zA-Z0-9 ]+$/;
-    let regex_descripcion = /^[a-zA-Z0-9 ]+$/;
+    //let regex_direccion = /^[a-zA-Z0-9 ]+$/;
+    //let regex_descripcion = /^[a-zA-Z0-9 ]+$/;
     //let regex_imagen = /.*\.(jpe?g|png)$/igm;
     let campos_requeridos = document.querySelectorAll(":required");
 
@@ -79,12 +90,12 @@ const validar = () => {
         }
     });
 
-    if (!regex_nombre.test(input_nombre.value)) {
-        error = true;
-        input_nombre.classList.add(`error-input`);
-    } else {
-        input_nombre.classList.remove(`error-input`);
-    }
+    //if (!regex_nombre.test(input_nombre.value)) {
+    //    error = true;
+    //    input_nombre.classList.add(`error-input`);
+    //} else {
+    //    input_nombre.classList.remove(`error-input`);
+    //}
 
     if (!regex_costo.test(input_costo.value)) {
         error = true;
@@ -100,19 +111,19 @@ const validar = () => {
         input_capacidad.classList.remove(`error-input`);
     }
 
-    if (!regex_direccion.test(input_direccion.value)) {
-        error = true;
-        input_direccion.classList.add(`error-input`);
-    } else {
-        input_direccion.classList.remove(`error-input`);
-    }
+    //if (!regex_direccion.test(input_direccion.value)) {
+    //    error = true;
+    //    input_direccion.classList.add(`error-input`);
+    //} else {
+    //    input_direccion.classList.remove(`error-input`);
+    //}
 
-    if (!regex_descripcion.test(input_descripcion.value)) {
-        error = true;
-        input_descripcion.classList.add(`error-input`);
-    } else {
-        input_descripcion.classList.remove(`error-input`);
-    }
+    //if (!regex_descripcion.test(input_descripcion.value)) {
+    //    error = true;
+    //    input_descripcion.classList.add(`error-input`);
+    //} else {
+    //    input_descripcion.classList.remove(`error-input`);
+    //}
 
     //if (!regex_imagen.test(input_imagen.value)) {
     //    error = true;
@@ -131,5 +142,9 @@ const validar = () => {
         });
     }
 };
+
+input_imagen.addEventListener('click', () => {
+    widgetCloudinary.open();
+}, false);
 
 btn_enviar.addEventListener('click', validar);
