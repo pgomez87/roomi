98bf8cfd1f3dd8fa50bc2e8a9259054e51c6fb2d
yosycastro <74128394+yosycastro@users.noreply.github.com>
2020-12-14 22:23:05 -0600

    Vamo'a ver

--- public/js/inicio-sesion-controlador.js
+++ public/js/inicio-sesion-controlador.js
@@ -3,7 +3,8 @@
 //para poder elegir un elemento:
 //const botonEnviar = document.getElementById('btn-enviar');
 //const botonEnviar = document.querySelector('#btn-enviar'); <--- mejor
-
+const btn_iniciar = document.querySelector('#btn-iniciar');
+const btn_recuperar = document.querySelector('#btn-olvido-contrasenna');
 const botonEnviar = document.querySelector('#btn-enviar');
 const textoEmail = document.querySelector('#txt-email')
 const textoContrasena = document.querySelector('#txt-contrasena');
@@ -32,7 +33,11 @@ const limpiar = () => {
     textoEmail.value = '';
     textoContrasena.value = '';
 }
-
+const obtener_datos = async() => {
+    let correo = document.querySelector('#txt-correo').value;
+    let contrasenna = document.querySelector('#txt-contrasenna').value;
+    iniciar_sesion(correo, contrasenna);
+};
 
 function obtenerDatos() {
     let nombre = textoEmail.value;
@@ -46,47 +51,76 @@ function obtenerDatos() {
 }
 
 const validar = () => {
-    let error = false;
-    let regexEmail = /^[a-zA-Z0-9]+@{1}[a-zA-Z]+(.com|.net.org.ac.cr)$/;
-    let regexContrasena = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
-    let camposRequeridos = document.querySelectorAll(':required');
+        let error = false;
+        let regexEmail = /^[a-zA-Z0-9]+@{1}[a-zA-Z]+(.com|.net.org.ac.cr)$/;
+        let regexContrasena = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+        let camposRequeridos = document.querySelectorAll(':required');
 
-    camposRequeridos.forEach(campo => {
-        if (campo.value == '') {
+        camposRequeridos.forEach(campo => {
+            if (campo.value == '') {
+                error = true;
+                campo.classList.add('error-input');
+            } else {
+                error = false;
+                campo.classList.remove('error-input');
+            }
+        });
+
+        if (!regexEmail.test(textoEmail.value)) {
             error = true;
-            campo.classList.add('error-input');
+            textoEmail.classList.add('error-input');
         } else {
             error = false;
-            campo.classList.remove('error-input');
+            textoEmail.classList.remove('error-input');
+        }
+
+        if (!regexContrasena.test(textoContrasena.value)) {
+            error = true;
+            textoContrasena.classList.add('error-input');
+        } else {
+            error = false;
+            textoContrasena.classList.remove('error-input');
+        }
+
+        if (error == false) {
+            obtenerDatos();
+        } else {
+            Swal.fire({
+                'title': 'No se pudo iniciar sesión',
+                'icon': 'warning',
+                'text': 'Por favor revise los campos resaltados'
+            });
+        }
+    }
+    /*Yoss hizo esto*/
+const mostrar_modal_recuperar = async() => {
+    const { value: formValues } = await Swal.fire({
+        title: 'Ingrese su correo electrónico para recuperar la contraseña',
+        html: `<div>
+        <label for="txt-correo">Correo</label>
+        <input type="text" id="txt-correo-reestablecer" class="swal2-input">
+        </div>`,
+        focusConfirm: false,
+        preConfirm: () => {
+            return [
+                document.getElementById('txt-correo-reestablecer').value
+            ]
         }
     });
 
-    if (!regexEmail.test(textoEmail.value)) {
-        error = true;
-        textoEmail.classList.add('error-input');
-    } else {
-        error = false;
-        textoEmail.classList.remove('error-input');
-    }
-
-    if (!regexContrasena.test(textoContrasena.value)) {
-        error = true;
-        textoContrasena.classList.add('error-input');
-    } else {
-        error = false;
-        textoContrasena.classList.remove('error-input');
-    }
-
-    if (error == false) {
-        obtenerDatos();
-    } else {
-        Swal.fire({
-            'title': 'No se pudo iniciar sesión',
-            'icon': 'warning',
-            'text': 'Por favor revise los campos resaltados'
-        });
-    }
-}
-
+    if (formValues) {
+        const { value: accept } = await Swal.fire({
+            icon: 'warning',
+            text: '¿Está seguro que desea reestablecer la contraseña?',
+            showCancelButton: true,
+            confirmButtonText: 'Sí'
+        })
+        if (accept) {
+            // Servicio
+            reestablecer_contrasenna(formValues[0]);
+        }
+    };
+};
 
 botonEnviar.addEventListener('click', validar);
+btn_recuperar.addEventListener('click', mostrar_modal_recuperar);
