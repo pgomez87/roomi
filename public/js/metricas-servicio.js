f567f8b6e3748e46c26e2a9cd6a769fe1cb81ab9
rgonzalezs93 <71414956+rgonzalezs93@users.noreply.github.com>
2020-12-18 13:36:02 -0600

    nnnnnnnnoooooooooooo

--- public/js/metricas-servicio.js
+++ public/js/metricas-servicio.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const listar_metricas_usuarios = async() => {
+    let lista_metricas = [];
+    await axios({
+        method: 'get',
+        url: 'http://localhost:3000/api/listar-usuarios',
+        responseType: 'json'
+    })
+}
