09f4c3294a7ef7ad52b971ec2a2dd678061cc2e6
rgonzalezs93 <71414956+rgonzalezs93@users.noreply.github.com>
2020-12-17 21:55:40 -0600

    u

--- public/js/listar-proveedores.js
+++ public/js/listar-proveedores.js
@@ -23,24 +23,121 @@ const mostrar_proveedores = async() => {
         let fila_internet = tabla_internet.insertRow();
         let fila_cable = tabla_cable.insertRow();
 
+
+
         if (proveedor.servicio.includes('Agua')) {
             fila_agua.insertCell().innerHTML = proveedor.nombre;
+            let celda_opcion = fila_agua.insertCell();
+
+            let boton_eliminar = document.createElement('button');
+            boton_eliminar.type = 'button';
+            boton_eliminar.innerText = 'Eliminar';
+            boton_eliminar.addEventListener('click', () => {
+                Swal.fire({
+                    icon: 'warning',
+                    text: 'Esta seguro que desea eliminar el proveedor?',
+                    showCancekButton: true,
+                    confirmButtonText: 'Si'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        eliminar_proveedores(proveedor._id)
+                    }
+                })
+            });
+
+            celda_opcion.appendChild(boton_eliminar);
         }
 
         if (proveedor.servicio.includes('Electricidad')) {
             fila_electricidad.insertCell().innerHTML = proveedor.nombre;
+            let celda_opcion = fila_electricidad.insertCell();
+
+            let boton_eliminar = document.createElement('button');
+            boton_eliminar.type = 'button';
+            boton_eliminar.innerText = 'Eliminar';
+            boton_eliminar.addEventListener('click', () => {
+                Swal.fire({
+                    icon: 'warning',
+                    text: 'Esta seguro que desea eliminar el proveedor?',
+                    showCancekButton: true,
+                    confirmButtonText: 'Si'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        eliminar_proveedores(proveedor._id)
+                    }
+                })
+            });
+
+            celda_opcion.appendChild(boton_eliminar);
         }
 
         if (proveedor.servicio.includes('telefono')) {
             fila_telefono.insertCell().innerHTML = proveedor.nombre;
+            let celda_opcion = fila_telefono.insertCell();
+
+            let boton_eliminar = document.createElement('button');
+            boton_eliminar.type = 'button';
+            boton_eliminar.innerText = 'Eliminar';
+            boton_eliminar.addEventListener('click', () => {
+                Swal.fire({
+                    icon: 'warning',
+                    text: 'Esta seguro que desea eliminar el proveedor?',
+                    showCancekButton: true,
+                    confirmButtonText: 'Si'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        eliminar_proveedores(proveedor._id)
+                    }
+                })
+            });
+
+            celda_opcion.appendChild(boton_eliminar);
         }
 
         if (proveedor.servicio.includes('Internet')) {
             fila_internet.insertCell().innerHTML = proveedor.nombre;
+            let celda_opcion = fila_internet.insertCell();
+
+            let boton_eliminar = document.createElement('button');
+            boton_eliminar.type = 'button';
+            boton_eliminar.innerText = 'Eliminar';
+            boton_eliminar.addEventListener('click', () => {
+                Swal.fire({
+                    icon: 'warning',
+                    text: 'Esta seguro que desea eliminar el proveedor?',
+                    showCancekButton: true,
+                    confirmButtonText: 'Si'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        eliminar_proveedores(proveedor._id)
+                    }
+                })
+            });
+
+            celda_opcion.appendChild(boton_eliminar);
         }
 
         if (proveedor.servicio.includes('Cable')) {
             fila_cable.insertCell().innerHTML = proveedor.nombre;
+            let celda_opcion = fila_cable.insertCell();
+
+            let boton_eliminar = document.createElement('button');
+            boton_eliminar.type = 'button';
+            boton_eliminar.innerText = 'Eliminar';
+            boton_eliminar.addEventListener('click', () => {
+                Swal.fire({
+                    icon: 'warning',
+                    text: 'Esta seguro que desea eliminar el proveedor?',
+                    showCancekButton: true,
+                    confirmButtonText: 'Si'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        eliminar_proveedores(proveedor._id)
+                    }
+                })
+            });
+
+            celda_opcion.appendChild(boton_eliminar);
         }
 
 
