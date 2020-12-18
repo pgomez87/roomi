56c0bbfb0dd73c43b1aba6299e7fa489310ac4b4
AndresJaen <74122497+AndresJaen@users.noreply.github.com>
2020-12-18 03:55:13 -0600

    integración facturas y noticias en todos los dashboards

--- public/js/dashboard-usuario.js
+++ public/js/dashboard-usuario.js
@@ -1,18 +1,3 @@
-'use strict';
-
-
-const boton_ver_tareas = document.querySelector('#boton-ver-tareas');
-const boton_ver_facturas = document.querySelector('#boton-ver-facturas');
-
-boton_ver_tareas.addEventListener('click', () => {
-    window.location.href = 'tareas-coordinador.html';
-});
-
-boton_ver_facturas.addEventListener('click', () => {
-    window.location.href = 'facturas-coordinador.html';
-});
-
-// listado de noticias dashboard coordinador
 const tablaNoticiasDash = document.querySelector('#lista-usuarios tbody');
 tablaNoticiasDash.innerHTML = '';
 
@@ -23,7 +8,6 @@ const noticiasDashboard = async() => {
     noticiasDash.forEach((noticia) => {
         let fila = tablaNoticiasDash.insertRow();
         fila.insertCell().innerHTML = noticia.titulo;
-
     });
 
     // console.log(noticiasDash);
@@ -50,6 +34,5 @@ const facturasDashoard = async() => {
     })
 }
 
-
-facturasDashoard();
 noticiasDashboard();
+facturasDashoard();
