691a2181493c7b108ffcd6e3fbe9d5ef97697da3
Ian-Asch <iascht@ucenfotec.ac.cr>
2020-12-18 00:32:56 -0600

    un pichaso de cosas

--- public/js/companeros-vivienda.js
+++ public/js/companeros-vivienda.js
@@ -2,24 +2,28 @@
 
 const div_companeros = document.querySelector('.container-companeros');
 
-let lista_companeros = await listar_usuarios();
+const conseguir_lista = async() => {
+    let lista_companeros = await listar_usuarios();
+    
+    lista_companeros.forEach(usuario => {
+        div_companeros.innerHTML = '';
+    
+        let card = document.createElement('div');
+        card.classList.add('tarjeta-companeros');
+    
+        let foto = document.createElement('img');
+        foto.classList.add('foto-companero');
+    
+        let nombre = document.createElement('h3');
+        nombre.innerText = usuario.nombre;
+    
+        let info = document.createElement('p');
+    
+        card.appendChild(foto);
+        card.appendChild(nombre);
+        card.appendChild(info);
+        div_companeros.appendChild(card);
+    });
+}
 
-lista_companeros.forEach(usuario => {
-    div_companeros.innerHTML = '';
-
-    let card = document.createElement('div');
-    card.classList.add('tarjeta-companeros');
-
-    let foto = document.createElement('img');
-    foto.classList.add('foto-companero');
-
-    let nombre = document.createElement('h3');
-    nombre.innerText = usuario.nombre;
-
-    let info = document.createElement('p');
-
-    card.appendChild(foto);
-    card.appendChild(nombre);
-    card.appendChild(info);
-    div_companeros.appendChild(card);
-});
+conseguir_lista();
