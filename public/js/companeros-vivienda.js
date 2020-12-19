a6195d2ace3cd97e91ff5eef0280121be5f12a9f
Ian-Asch <iascht@ucenfotec.ac.cr>
2020-12-18 20:02:02 -0600

    Update companeros-vivienda.js

--- public/js/companeros-vivienda.js
+++ public/js/companeros-vivienda.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const lista_descripciones = ['Me gusta mucho coleccionar jackets amarillas. Trabajo con maquinaria compleja. Ultimamente mi vida se ha desordenado, pero planeo poner todo en su lugar en el futuro. He tenido problemas para quedarme en un solo lugar mucho tiempo.','Me gusta mucho la fotografía y la música. Trabajo en diseño. Soy responsable y me gusta tener las cosas en orden. Soy alérgica al pelo de varios animales.','Me gusta el arte, particularmente la pintura Me dedico a la actuacion Ultimamente mi vida se ha desordenado, pero planeo poner todo en su lugar en el futuro. Soy alérgica al pelo de varios animales.','Me gusta mucho la música. Soy pianista profesional. Mi espacio puede estar un poco desordenada. Soy alérgica al pelo de varios animales.','Me gusta armar modelos y rompecabezas. Me gustan los video juegos Trabajo en mercadeo. Puedo ser muy desordenado, pero el desorden no sale de mi espacio Soy más nocturno'];
+
 const div_companeros = document.querySelector('.container-companeros');
 
 const conseguir_lista = async() => {
     let lista_companeros = await listar_usuarios();
+    let indice = 0;
     
     lista_companeros.forEach(usuario => {
         div_companeros.innerHTML = '';
@@ -18,6 +21,8 @@ const conseguir_lista = async() => {
         nombre.innerText = usuario.nombre;
     
         let info = document.createElement('p');
+        info.innerText = lista_descripciones[indice];
+        indice++;
     
         card.appendChild(foto);
         card.appendChild(nombre);
